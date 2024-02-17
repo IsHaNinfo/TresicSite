30deeace9e51a04370b06aefc590a08c9787e8d8
LakshanDayathilake <ldayathilake@icptechno.com>
2024-02-17 23:42:55 +0530

    chnaged photos and fotter

--- Tresic/src/Components/MyFooter.jsx
+++ Tresic/src/Components/MyFooter.jsx
@@ -1,65 +1,83 @@
-import React from 'react'
-import { Footer } from 'flowbite-react';
-import {  BsFacebook, BsGithub, BsInstagram, BsTwitter,BsLinkedin } from 'react-icons/bs'
+import React from "react";
+import { Footer } from "flowbite-react";
+import {
+  BsFacebook,
+  BsGithub,
+  BsInstagram,
+  BsTwitter,
+  BsLinkedin,
+} from "react-icons/bs";
 import Logo from "../assets/Images/Logo.png";
-import { FaWhatsapp, FaEnvelope } from 'react-icons/fa';
+import { FaWhatsapp, FaEnvelope } from "react-icons/fa";
 const MyFooter = () => {
   return (
     <Footer container>
-      <div className="w-full  shadow-lg bg-neutralSilver " id='footer' >
+      <div className="w-full  shadow-lg bg-neutralSilver " id="footer">
         <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1 rounded-md ">
-
-        <div className=' p-16 rounded-md   items-center justify-center w-[700px] h-[200px] '>
-                <div className='text-center'>
-          <a href='' className='text-2x1 font-semibold flex items-center justify-center space-x-3'>
-            <img src={Logo} alt='' className='w-12 inline-block items-center' />
-            <span className='text-brandPrimary text-2xl'> TRESIC TECHNOLOGY</span>
-          </a>
-          <p className="text-brandPrimary font-semibold">"Imagination come True"</p>
-        </div>
-                    <div className="flex space-x-16 mt-4 sm:mt-8 sm:justify-center">
-                    <a href="#" className='text-brandPrimary text-3xl'>
-        <BsFacebook />
-      </a>
-                <a href="#" className='text-brandPrimary text-3xl'>
-        <BsInstagram />
-      </a>
-      <a href="#" className='text-brandPrimary text-3xl'>
-        <BsLinkedin />
-      </a>
-
-        </div>
-
+          <div className=" p-16 rounded-md   items-center justify-center w-[700px] h-[200px] ">
+            <div className="text-center">
+              <a
+                href=""
+                className="text-2x1 font-semibold flex items-center justify-center space-x-3"
+              >
+                <img
+                  src={Logo}
+                  alt=""
+                  className="w-12 inline-block items-center"
+                />
+                <span className="text-brandPrimary text-2xl">
+                  {" "}
+                  TRESIC TECHNOLOGY
+                </span>
+              </a>
+              <p className="text-brandPrimary font-semibold">
+                "Imagination come True"
+              </p>
+            </div>
+            <div className="flex space-x-16 mt-4 sm:mt-8 sm:justify-center">
+              <a href="#" className="text-brandPrimary text-3xl">
+                <BsFacebook />
+              </a>
+              <a href="#" className="text-brandPrimary text-3xl">
+                <BsInstagram />
+              </a>
+              <a href="#" className="text-brandPrimary text-3xl">
+                <BsLinkedin />
+              </a>
+            </div>
           </div>
 
-          
-              <div className=" p-10 mt-6 rounded-md flex items-center justify-center w-[700px] h-[200px]">
-                <div>
-                  <Footer.Title title="Contact us" className='text-2xl text-brandPrimary'/>
-                  <Footer.LinkGroup col className="justify-end">
-                    <div className="flex items-center space-x-2 text-brandPrimary">
-                      <FaWhatsapp size={25} /> <Footer.Link href="tel:+0779615553 ">077 961 5553</Footer.Link>
-                    </div>
-                    <div className="flex items-center space-x-2 text-brandPrimary">
-                      <FaEnvelope size={25} /> <Footer.Link href="mailto:info.tresic@gmail.com">info.tresic@gmail.com</Footer.Link>
-                    </div>
-                  </Footer.LinkGroup>
+          <div className=" p-10 mt-6 rounded-md flex items-center justify-center w-[700px] h-[200px]">
+            <div>
+              <Footer.Title
+                title="Contact us"
+                className="text-2xl text-brandPrimary"
+              />
+              <Footer.LinkGroup col className="justify-end">
+                <div className="flex items-center space-x-2 text-brandPrimary">
+                  <FaWhatsapp size={25} />{" "}
+                  <Footer.Link href="tel:+0779615553 ">
+                    077 961 5553
+                  </Footer.Link>
                 </div>
-              </div>
-
-                
-          </div> 
-          <Footer.Divider/>
-          <div className=" sm:flex sm:items-center sm:justify-between">
-          <Footer.Copyright href="#" by="Tresic™" year={2024} /> 
-          <p className='text-neutralGrey'> All Rights Resverd</p>
+                <div className="flex items-center space-x-2 text-brandPrimary">
+                  <FaEnvelope size={25} />{" "}
+                  <Footer.Link href="mailto:info.tresic@gmail.com">
+                    info.tresic@gmail.com
+                  </Footer.Link>
+                </div>
+              </Footer.LinkGroup>
+            </div>
           </div>
         </div>
-      
-          
-      
+        <Footer.Divider />
+        <div className=" sm:flex sm:items-center sm:justify-between mb-6 p-10">
+          <Footer.Copyright href="#" by="Tresic™" year={2024} />
+          <p className="text-neutralGrey"> All Rights Resverd</p>
+        </div>
+      </div>
     </Footer>
-  )
-}
+  );
+};
 
-export default MyFooter
+export default MyFooter;
--- Tresic/src/Components/Projects.jsx
+++ Tresic/src/Components/Projects.jsx
@@ -1,107 +1,120 @@
-import React from 'react'
-import { Button, Modal } from 'flowbite-react';
-import { useState } from 'react';
-import { Carousel } from 'flowbite-react';
+import React from "react";
+import { Button, Modal } from "flowbite-react";
+import { useState } from "react";
+import { Carousel } from "flowbite-react";
 
 const Project = () => {
-    const [openModal, setOpenModal] = useState(false);
-    const [currentProject, setCurrentProject] = useState(null);
+  const [openModal, setOpenModal] = useState(false);
+  const [currentProject, setCurrentProject] = useState(null);
 
-    const handleReadMoreClick = (projectId) => {
-      setCurrentProject(projectId);
-      setOpenModal(true);
-    };
-  
-    const handleCloseModal = () => {
-      setCurrentProject(null);
-      setOpenModal(false);
-    };
+  const handleReadMoreClick = (projectId) => {
+    setCurrentProject(projectId);
+    setOpenModal(true);
+  };
 
+  const handleCloseModal = () => {
+    setCurrentProject(null);
+    setOpenModal(false);
+  };
 
-    const projects =[
-        {id:1,
-        title:"Shop Fushion Enterprise application",
-        image:"src/assets/projects/web12.png",
-        tech:[
-            "src/assets/projects/web12.png",
-            "src/assets/projects/web13.png",
-            "src/assets/projects/web14.png",
-            "src/assets/projects/web15.png",
-            "src/assets/projects/web16.png",
-            "src/assets/projects/web17.png"
-        ]
-        },
-        {id:2,
-            title:"Lenzz Software Project Management System", 
-            image: "src/assets/projects/web1.png",
-            tech:[
-                " src/assets/projects/web1.png",
-                "src/assets/projects/web2.png",
-                "src/assets/projects/web3.png",
-                "src/assets/projects/web4.png",
-                "src/assets/projects/web5.png",
-                "src/assets/projects/web6.png"
-            ]
-         },
-     {id:3,
-                title:"Mobile App Development",
-                image: "src/assets/projects/Mobile/app.png",
-                tech:[
-                  "src/assets/projects/Mobile/app.png",
-                  "src/assets/projects/Mobile/app 2.png",
-                    
-                ]
-      },
-      {id:4,
-        title:"UI/UX Design",
-        image:"src/assets/projects/UI/UI.png",
-        tech:[
-          "src/assets/projects/UI/UI.png",
-          "src/assets/projects/UI/UI2.png",
-          "src/assets/projects/UI/UI3.png",
-          "src/assets/projects/UI/UI4.png",
-          "src/assets/projects/UI/UI5.png",
-          "src/assets/projects/UI/UI6.png",  
-          "src/assets/projects/UI/UI7.png"
-        ]
-          
-        },
-        {id:5,
-            title:"Enterprise application",
-            image:"src/assets/images/Rent.png",
-            tech:[
-                "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg",
-                "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
-                "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg"
-            ]
-         },
-     {id:6,
-                title:"Enterprise application",
-                image:"src/assets/images/Rent.png",
-                tech:[
-                    "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg",
-                    "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg",
-                    "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg"
-                ]
-      }
-    ]
-
+  const projects = [
+    {
+      id: 1,
+      title: "Shop Fushion Enterprise application",
+      image: "src/assets/projects/web14.png",
+      tech: [
+        "src/assets/projects/web14.png",
+        "src/assets/projects/web15.png",
+        "src/assets/projects/web12.png",
+        "src/assets/projects/web13.png",
+        "src/assets/projects/web16.png",
+        "src/assets/projects/web17.png",
+      ],
+    },
+    {
+      id: 2,
+      title: "Project Management Platform",
+      image: "src/assets/projects/web18.png",
+      tech: [" src/assets/projects/web18.png", "src/assets/projects/web19.png"],
+    },
+    {
+      id: 3,
+      title: "Mobile App Development",
+      image: "src/assets/projects/Mobile/app.png",
+      tech: [
+        "src/assets/projects/Mobile/app.png",
+        "src/assets/projects/Mobile/app 2.png",
+      ],
+    },
+    {
+      id: 4,
+      title: "Power Astronova",
+      image: "src/assets/projects/web (7).png",
+      tech: [
+        "src/assets/projects/web (7).png",
+        "src/assets/projects/web (8).png",
+        "src/assets/projects/web (9).png",
+      ],
+    },
+    {
+      id: 5,
+      title: "UI/UX Design",
+      image: "src/assets/projects/UI/UI.png",
+      tech: [
+        "src/assets/projects/UI/UI.png",
+        "src/assets/projects/UI/UI2.png",
+        "src/assets/projects/UI/UI3.png",
+        "src/assets/projects/UI/UI4.png",
+        "src/assets/projects/UI/UI5.png",
+        "src/assets/projects/UI/UI6.png",
+        "src/assets/projects/UI/UI7.png",
+      ],
+    },
+  ];
 
   return (
-    <div className='px-4 lg:px-14 max-w-screen-2xl mx-auto my-12 ' id='Portfolio'>
-      <div className='text-center md:w-1/2 mx-auto'>
-            <h2 className='text-4xl text-neutralDGrey font-semibold mb-4'> Engage with our new projects </h2>
-            <p className='text-sm text-neutralDGrey mb-8 md:w-3/4 mx-auto'>The Nextcent blog is the best place 
-                to read about the latest membership insights, trends and more. See who's joining the community, read about how our community are increasing their membership income and lot's more.​</p>
-        </div>
-      <div className='grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-8 items-center justify-between'>
+    <div
+      className="px-4 lg:px-14 max-w-screen-2xl mx-auto my-12 "
+      id="Portfolio"
+    >
+      <div className="text-center md:w-1/2 mx-auto">
+        <h2 className="text-4xl text-neutralDGrey font-semibold mb-4">
+          {" "}
+          Engage with our new projects{" "}
+        </h2>
+        <p className="text-sm text-neutralDGrey mb-8 md:w-3/4 mx-auto">
+          Welcome to our digital space where creativity meets innovation. At
+          Tresic Technologies we're passionate about crafting exceptional
+          experiences and solutions that leave a lasting impact. With a blend of
+          expertise, dedication, and forward-thinking strategies, we strive to
+          exceed expectations, pushing boundaries to deliver results that
+          resonate. Explore our portfolio to witness the fusion of artistry and
+          technology, driving success for our clients and inspiring the future
+          of digital excellence.
+        </p>
+      </div>
+      <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-8 items-center justify-between">
         {projects.map((project) => (
-          <div key={project.id} className='mx-auto relative mb-12 cursor-pointer rounded-sm'>
-            <img src={project.image} alt="" className='hover:scale-95 transition-all duration-300 rounded-sm' />
-            <div className='text-center px-2 py-3 bg-white shadow-lg rounded-md md:w-3/4 mx-auto absolute left-0 right-0 -bottom-12'>
-              <h3 className='mb-3 text-neutralGrey font-semibold'>{project.title}</h3>
-              <div className='flex items-center justify-center gap-8'>
-                <button className='font-bold text-brandPrimary hover:text-neutral-700' onClick={() => handleReadMoreClick(project.id)}>Read more</button>
+          <div
+            key={project.id}
+            className="mx-auto relative mb-12 cursor-pointer rounded-sm"
+          >
+            <img
+              src={project.image}
+              alt=""
+              className="hover:scale-95 transition-all duration-300 rounded-sm"
+            />
+            <div className="text-center px-2 py-3 bg-white shadow-lg rounded-md md:w-3/4 mx-auto absolute left-0 right-0 -bottom-12">
+              <h3 className="mb-3 text-neutralGrey font-semibold">
+                {project.title}
+              </h3>
+              <div className="flex items-center justify-center gap-8">
+                <button
+                  className="text-brandPrimary hover:text-neutral-400"
+                  onClick={() => handleReadMoreClick(project.id)}
+                >
+                  View more
+                </button>
               </div>
             </div>
           </div>
@@ -109,24 +122,33 @@ const Project = () => {
       </div>
 
       {/* Single Carousel for the clicked project */}
-      <Modal show={openModal}     onClose={() => setOpenModal(false)}>
-        <Modal.Header>{currentProject && projects.find((project) => project.id === currentProject)?.title}</Modal.Header>
+      <Modal show={openModal} onClose={() => setOpenModal(false)}>
+        <Modal.Header>
+          <span cla>
+            {currentProject &&
+              projects.find((project) => project.id === currentProject)?.title}
+          </span>
+        </Modal.Header>
         <Modal.Body>
-          <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+          <div className="h-56 sm:h-40 xl:h-80 2xl:h-96">
             <Carousel>
               {currentProject &&
                 projects
                   .find((project) => project.id === currentProject)
                   ?.tech.map((tech, index) => (
-                    <img key={index} src={tech} alt={`Tech ${index}`} className="h-100 w-100 object-cover" />
+                    <img
+                      key={index}
+                      src={tech}
+                      alt={`Tech ${index}`}
+                      className="h-100 w-100 object-cover"
+                    />
                   ))}
             </Carousel>
           </div>
         </Modal.Body>
-        <Modal.Footer>{/* Add content for the modal footer if needed */}</Modal.Footer>
       </Modal>
     </div>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
